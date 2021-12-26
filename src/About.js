4d2a01c37fe464f93dcf329dcfb9abf881f33f31
Veera <veeranarappa1@gmail.com>
2021-12-26 22:04:31 +0530

    Changed aboutus

--- src/About.js
+++ src/About.js
@@ -7,8 +7,8 @@ class About extends React.Component{
     }
     render(){
         return(
-            <div>
-                <h1> About Page  </h1>
+            <div> 
+                <h1> About Page 1234567  </h1>
                 <h1> About Page  </h1>
                 <h1> About Page  </h1>
                 <h1> About Page  </h1>
